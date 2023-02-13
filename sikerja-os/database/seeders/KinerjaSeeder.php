04f12ebb9e57655b8a36cde657498547dee239ec
Erma YC Saragih <ermayuliclara@gmail.com>
2023-02-13 14:38:28 +0700

    Seeder Kinerja untuk Dummy

--- sikerja-os/database/seeders/KinerjaSeeder.php
+++ sikerja-os/database/seeders/KinerjaSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Kinerja;
+use Illuminate\Support\Facades\DB;
 
 class KinerjaSeeder extends Seeder
 {
@@ -13,6 +15,26 @@ class KinerjaSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Kinerja::create([
+            'id_user' => 4,
+            'id_sub' => 2,
+            'id_status' => 1,
+            'kinerja' => 'Testing Prodeskel',
+            'tgl_mulai' => '2022-01-11',
+            'tgl_selesai' => '2022-01-11',
+            'jam_mulai' => '08:30',
+            'jam_selesai' => '10:30',
+        ]);
+
+        Kinerja::create([
+            'id_user' => 4,
+            'id_sub' => 2,
+            'id_status' => 1,
+            'kinerja' => 'Membuat Surat Nota Dinas',
+            'tgl_mulai' => '2022-01-11',
+            'tgl_selesai' => '2022-01-11',
+            'jam_mulai' => '10:30',
+            'jam_selesai' => '11:30',
+        ]);
     }
 }

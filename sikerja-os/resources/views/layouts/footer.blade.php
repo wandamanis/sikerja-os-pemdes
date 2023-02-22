7eeae43cfb21ef50e692d7de564eafd4283b965c
Muchlas <muchlas.muchlas@gmail.com>
2023-02-22 13:58:19 +0700

    Minor code changes and clean up

--- sikerja-os/resources/views/layouts/footer.blade.php
+++ sikerja-os/resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2022 <a href="#"></a></strong>
+    <strong>Copyright &copy; {{ \Carbon\Carbon::now()->format('Y') }} <a href="#"></a></strong>
     Direktorat Jeneral Bina Pemerintahan Desa
     <div class="float-right d-none d-sm-inline-block">
         <b>Version</b> 1.0
--- sikerja-os/resources/views/layouts/sidebar.blade.php
+++ sikerja-os/resources/views/layouts/sidebar.blade.php
@@ -31,17 +31,6 @@
         </div>
     </div>
 
-    <div class="form-inline">
-        {{-- <div class="input-group" data-widget="sidebar-search">
-            <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-            <div class="input-group-append">
-                <button class="btn btn-sidebar">
-                    <i class="fas fa-search fa-fw"></i>
-                </button>
-            </div>
-        </div> --}}
-    </div>
-
     <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
             @if (auth()->user()->id_level === 3)
